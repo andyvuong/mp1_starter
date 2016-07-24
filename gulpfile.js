2dc2909c588e857c42ded7c85dfc310bdc1edaa1
Sujay Khandekar <khandek2@illinois.edu>
2016-07-23 18:43:39 -0700

    getting some gulp file setup

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('default', function () {
+  // place code for your default task here
+});
+
+gulp.task('styles', function () {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
+
+//Watch task
+gulp.task('default', function () {
+    gulp.watch('sass/**/*.scss', ['styles']);
+});
